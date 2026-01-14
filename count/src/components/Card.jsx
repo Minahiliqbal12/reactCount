d74ff84a2dd67ab66b1ecfc012e75d7180fd6b4f
Minahil Iqbal <manoiqbal97@gmail.com>
2026-01-14 19:57:05 +0500

    Update Card.jsx

--- count/src/components/Card.jsx
+++ count/src/components/Card.jsx
@@ -15,21 +15,21 @@ const Card = () => {
   }
 
   return (
-    <div className="h-screen w-1/2 flex items-center justify-center bg-gray-100">
+    <div className="h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-pink-200 p-6 rounded-xl shadow-lg w-90 text-center">
         <h1 className="text-4xl font-bold text-blue-600 mb-7">{num}</h1>
 
         <div className="flex justify-between ">
           <button
             onClick={decreased}
-            className="px-4 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
+            className="px-2 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
           >
             Decrease
           </button>
 
           <button
             onClick={increased}
-            className="px-4 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
+            className="px-2 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
           >
             Increase
           </button>
