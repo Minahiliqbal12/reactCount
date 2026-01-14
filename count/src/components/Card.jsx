966decd6528d5d8c28053435313080b997912f40
Minahil Iqbal <manoiqbal97@gmail.com>
2026-01-14 20:17:04 +0500

    Update Card.jsx

--- count/src/components/Card.jsx
+++ count/src/components/Card.jsx
@@ -22,20 +22,20 @@ const Card = () => {
         <div className="flex justify-center gap-3 ">
           <button
             onClick={decreased}
-            className="px-7 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
+            className="px-4 w-28 h-12 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
           >
             Decrease
           </button>
 
           <button
             onClick={increased}
-            className="px-7 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
+            className="px-4 py-2 w-28 h-12 bg-amber-500 text-white rounded-lg hover:bg-amber-600"
           >
             Increase
           </button>
           <button
             onClick={restart}
-            className="px-7 py-2   bg-amber-500 text-white rounded-lg hover:bg-amber-600"
+            className="px-4 py-2 w-28 h-12  bg-amber-500 text-white rounded-lg hover:bg-amber-600"
           >
             Restart
           </button>
